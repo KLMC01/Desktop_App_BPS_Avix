416193c71784fc75d0004f24c9179cd746c156d8
KLMC <chandimanesh@gmal.com>
2026-05-12 00:52:39 +0530

    commit

--- Avixlk.py
+++ Avixlk.py
@@ -5,14 +5,18 @@ window = Tk()
 window.title ('Avix Mobile')
 
 
+
 lbl = Label (window,text = "Hello", font = ("Arial", 75))
 lbl.grid(column =0, row = 2)
 
 def clicked():
-    lbl.configure(text = "Hi Everyone!!")
+    res = "Welcome " + txt.get()
+    lbl.configure(text = res)
 
+txt = Entry(window, width = 15)
+txt.grid(column = 2, row = 2)
 
-btn = Button(window, text = 'Click me', bg = "red", fg = "white", command = Clicked)
+btn = Button(window, text = 'Click me', bg = "red", fg = "white", command = clicked)
 btn.grid(column =2, row = 2)
 
 window.mainloop()
